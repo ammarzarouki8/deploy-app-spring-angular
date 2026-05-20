476f1ea143e240fa41a30f74af72f31cce1bde82
ammar <ammarzarouki8@gmail.com>
2026-05-20 17:14:12 +0000

    Fix backend URL and headers in Angular service

--- angular-app/src/app/services/user.service.ts
+++ angular-app/src/app/services/user.service.ts
@@ -1,25 +1,26 @@
+cat > angular-app/src/app/services/user.service.ts << 'EOF'
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, last } from 'rxjs';
+import { Observable } from 'rxjs';
 import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  baseURL:string="http://192.168.43.166:32117/user"
 
-  baseURL:string="http://192.168.187.7:8081/user"
-  constructor(private  httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient) { }
 
   getAllUsers():Observable<any>{
     let headers = new HttpHeaders();
-    headers.append("Access-Control-Allow-Origin", "*")
+    headers = headers.append("Access-Control-Allow-Origin", "*")
     return this.httpClient.get(this.baseURL + "/all",{headers})
   }
 
   deleteUser(id:string):Observable<any>{
     let headers = new HttpHeaders();
-    headers.append("Access-Control-Allow-Origin", "*")
+    headers = headers.append("Access-Control-Allow-Origin", "*")
     return this.httpClient.delete(this.baseURL + "/delete/"+id,{headers})
   }
 
@@ -27,7 +28,8 @@ export class UserService {
     let body = { firstName: firstname, lastName: lastname };
     let url=this.baseURL + "/add";
     let headers = new HttpHeaders();
-    headers.append("Access-Control-Allow-Origin", "*")
+    headers = headers.append("Access-Control-Allow-Origin", "*")
     return this.httpClient.post(url,body,{headers})
   }
 }
+EOF
